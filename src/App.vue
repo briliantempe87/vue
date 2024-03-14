c2a730d7af8fda1011b950c93e3d7031a9b92f8f
briliant firmansyah <briliantempe87.com>
2024-03-14 07:10:56 +0700

    mamperbaiki error

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>IDShop</h1>
-    <p class="animate__fadeInDown">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quasi dolores sunt dolorem omnis amet eaque odio! Velit eaque culpa, adipisci molestiae sed nihil, totam quisquam ad ut aspernatur architecto.</p>
+    <p class="animate__fadeInLeft">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quasi dolores sunt dolorem omnis amet eaque odio! Velit eaque culpa, adipisci molestiae sed nihil, totam quisquam ad ut aspernatur architecto.</p>
     <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
